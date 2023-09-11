07a1f9951deeb43da2d7a180ecad13a859e6029f
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-11 09:25:28 +0600

    some minor tweaks

--- src/VisitedCountries.jsx
+++ src/VisitedCountries.jsx
@@ -2,7 +2,7 @@ import VisitedCountry from "./components/VisitedCountry";
 
 const VisitedCountries = ({traveledCountries}) => {
     return (
-        <div style={{display: 'flex', gap: '20px'}}>
+        <div style={{display: 'flex', gap: '20px', marginTop: '10px'}}>
             {
                 traveledCountries.map((visitedCountry, index) => <VisitedCountry key={index} visitedCountry={visitedCountry}></VisitedCountry>)
             }
