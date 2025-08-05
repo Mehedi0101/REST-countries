8cefe4099e28c5c63359c06079d0d2ae222a1a44
Mehedi0101 <mehedi29121998@gmail.com>
2025-08-05 12:56:25 +0600

    api updated

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -7,7 +7,7 @@ const Countries = ({traveledCountries,setTraveledCountries}) => {
     const [countryList, setCountryList] = useState([]);
 
     useEffect( () => {
-        fetch('https://restcountries.com/v3.1/all')
+        fetch('https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region')
             .then( res => res.json())
             .then( data => setCountryList(data))
     }, [] )
